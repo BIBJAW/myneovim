6c04954a35bc0fc8c50bbd57c0b9cd9400d80735
MD.Ahsan Habib <68529306+BIBJAW@users.noreply.github.com>
2023-05-14 19:58:46 +0600

    Add files via upload

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,3 +1,8 @@
-require("notify").setup({
+vim.notify = require("notify")
+vim.notify.setup({
+	minimum_width = 30,
 	background_colour = "#00000000",
+	stages = "fade",
+	timeout = 2000,
+	fps = 60,
 })
